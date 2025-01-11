55d54196c8c9a56a96d8a4d67eee82fdbaf06241
jesus <kids.anguita@gmail.com>
2025-01-11 14:12:49 +0100

    Ajout des commentaires du premier test unitaire.

--- test_TestsUnitaires.py
+++ test_TestsUnitaires.py
@@ -1,9 +1,9 @@
-from MotDePasse import Generateur
+from MotDePasse import Generateur # Importation de la fonction Generateur du fichier MotDePasse.py
 
-def test_Peut_Generer_Mot_De_Passe():
+def test_Peut_Generer_Mot_De_Passe(): # Test de la fonction Generateur.
     
-    global Mdp
+    global Mdp # Déclaration de la variable Mdp en variable globale.
     
-    Mdp = Generateur(8, 2)
+    Mdp = Generateur(8, 2) # Appel de la fonction Generateur avec 8 caractères et 2 mots.
     
-    assert len(Mdp) == 8 * 2 + (1 * (2/2))
+    assert len(Mdp) == 8 * 2 + (1 * (2/2)) # 8 caractères * 2 mots + 1 tiret.
